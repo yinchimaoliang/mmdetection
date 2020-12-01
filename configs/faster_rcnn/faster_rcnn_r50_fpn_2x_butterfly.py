7daee17f66ce924350b70af75406f841e933f6e1
liyinhao <liyinhao@sensetime.com>
2020-12-01 19:49:47 +0800

    Able to train

---
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_2x_butterfly.py
@@ -0,0 +1,15 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/butterfly.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+
+model = dict(
+
+    roi_head=dict(
+        bbox_head=dict(
+            num_classes=94,
+            )))
+
+optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
+
