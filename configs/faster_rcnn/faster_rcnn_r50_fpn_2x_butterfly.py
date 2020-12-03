8227561ffc847b93d17c38c6c18bf2e97e56caf3
liyinhao <liyinhao@sensetime.com>
2020-12-03 14:50:11 +0800

    Change lr.

--- configs/faster_rcnn/faster_rcnn_r50_fpn_2x_butterfly.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_2x_butterfly.py
@@ -11,5 +11,5 @@ model = dict(
             num_classes=94,
             )))
 
-optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 
