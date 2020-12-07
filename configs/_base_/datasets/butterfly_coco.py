57129a3dff5c3c9bc0d20dab2ed6a915cb3a777a
liyinhao <liyinhao@sensetime.com>
2020-12-07 09:01:32 +0800

    Able to train coco.

--- configs/_base_/datasets/butterfly_coco.py
+++ configs/_base_/datasets/butterfly_coco.py
@@ -1,13 +1,12 @@
-dataset_type = 'VOCDataset'
+dataset_type = 'CocoDataset'
 data_root = 'data/butterfly/'
-classes = [str(i + 1) for i in range(94)]
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+classes = [str(i + 1) for i in range(94)]
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='Resize', img_scale=[(2048, 800), (2048, 1024)], keep_ratio=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -18,7 +17,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 1024),
+        img_scale=(1333, 800),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -30,23 +29,23 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'instances_train.json',
+        ann_file=data_root + 'annotations/instances_train.json',
         img_prefix=data_root + 'TrainData/',
         pipeline=train_pipeline,
         classes=classes),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'instances_val.json',
+        ann_file=data_root + 'annotations/instances_val.json',
         img_prefix=data_root + 'TrainData/',
         pipeline=test_pipeline,
         classes=classes),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'instances_val.json',
+        ann_file=data_root + 'annotations/instances_val.json',
         img_prefix=data_root + 'TrainData/',
         pipeline=test_pipeline,
         classes=classes))
--- configs/vfnet/vfnet_r50_fpn_1x_butterfly.py
+++ configs/vfnet/vfnet_r50_fpn_1x_butterfly.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/datasets/butterfly_voc.py',
+    '../_base_/datasets/butterfly_coco.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 # model settings
