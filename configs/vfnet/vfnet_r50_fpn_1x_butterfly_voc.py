13267b1916453684f3558617087f9775d7c71405
liyinhao <liyinhao@sensetime.com>
2020-12-08 08:49:01 +0800

    Change num_classes to 94.

--- configs/vfnet/vfnet_r50_fpn_1x_butterfly_voc.py
+++ configs/vfnet/vfnet_r50_fpn_1x_butterfly_voc.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/datasets/butterfly_coco.py',
+    '../_base_/datasets/butterfly_voc.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 # model settings
@@ -26,7 +26,7 @@ model = dict(
         relu_before_extra_convs=True),
     bbox_head=dict(
         type='VFNetHead',
-        num_classes=80,
+        num_classes=94,
         in_channels=256,
         stacked_convs=3,
         feat_channels=256,
@@ -92,8 +92,7 @@ data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(pipeline=train_pipeline),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline))
+    val=dict(pipeline=test_pipeline))
 
 # optimizer
 optimizer = dict(
--- configs/vfnet/vfnet_r50_fpn_mstrain_2x_butterfly.py
+++ configs/vfnet/vfnet_r50_fpn_mstrain_2x_butterfly.py
@@ -1,4 +1,4 @@
-_base_ = './vfnet_r50_fpn_1x_butterfly.py'
+_base_ = './vfnet_r50_fpn_1x_butterfly_voc.py'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 # learning policy
