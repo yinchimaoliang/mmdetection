6a966e6c36bd17384b4e027205daf9fd00971fbf
liyinhao <liyinhao0413@qq.com>
2020-12-11 21:10:00 +0800

    Able to generate data

--- tools/generate_data.py
+++ tools/generate_data.py
@@ -34,10 +34,10 @@ def _make_xml(target_path, obj_data, image_name):
     img = mmcv.imread(osp.join(target_path, 'JPEGImages', image_name))
     node_size = SubElement(node_root, 'size')
     node_width = SubElement(node_size, 'width')
-    node_width.text = img.shape[1]
+    node_width.text = str(img.shape[1])
 
     node_height = SubElement(node_size, 'height')
-    node_height.text = img.shape[0]
+    node_height.text = str(img.shape[0])
 
     node_depth = SubElement(node_size, 'depth')
     node_depth.text = '3'
@@ -55,9 +55,9 @@ def _make_xml(target_path, obj_data, image_name):
         node_ymin = SubElement(node_bndbox, 'ymin')
         node_ymin.text = str(obj_instance[1])
         node_xmax = SubElement(node_bndbox, 'xmax')
-        node_xmax.text = str(obj_instance[0] + obj_instance[2])
+        node_xmax.text = str(int(obj_instance[0]) + int(obj_instance[2]))
         node_ymax = SubElement(node_bndbox, 'ymax')
-        node_ymax.text = str(obj_instance[1] + obj_instance[3])
+        node_ymax.text = str(int(obj_instance[1]) + int(obj_instance[3]))
 
 
     xml = tostring(node_root, pretty_print = True)
@@ -82,6 +82,7 @@ def _copy_data(source_dir, target_dir):
 
 def _generate_ann(target_dir, annotations):
     annotations_dict = dict()
+    mmcv.mkdir_or_exist(osp.join(target_dir, 'Annotations'))
     for annotation in annotations:
         name, x, y, height, width, class_name = annotation.split(',')
         if name not in annotations_dict.keys():
@@ -89,17 +90,20 @@ def _generate_ann(target_dir, annotations):
         else:
             annotations_dict[name].append([x, y, height, width, class_name[:-1]])
 
-    for filename in annotations_dict.keys():
+    for i, filename in enumerate(annotations_dict.keys()):
+        if filename == '010008610797F9F5026D8B245A023F72AFBA0F76C702.jpg' or not osp.exists(osp.join(target_dir, 'JPEGImages', filename)) or filename == '010009AA7D8185BC63EA5FFBAED2D916A8DE1DC27602.jpg' or filename == '0100098862143E8B22ED715C1305DF63124F68439402.jpg':
+            continue
         dom = _make_xml(target_dir, annotations_dict[filename], filename)
-        print(dom)
-
+        with open(osp.join(target_dir, 'Annotations', filename.split('.')[0] + 'xml'), 'wb') as f:
+            f.write(dom.toprettyxml(encoding='utf-8'))
+        print(f'{filename} finished')
 
 def main():
     args = parse_args()
     source_dir = args.source_dir
     target_dir = args.target_dir
     annotations = _copy_data(source_dir, target_dir)
-    # _generate_ann(target_dir, annotations)
+    _generate_ann(target_dir, annotations)
 
 if __name__ == '__main__':
     main()
