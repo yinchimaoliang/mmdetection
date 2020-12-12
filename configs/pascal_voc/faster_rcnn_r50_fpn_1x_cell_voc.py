e74d13f8c573e4954404ab90a06087013fd24834
liyinhao <liyinhao0413@qq.com>
2020-12-12 18:06:29 +0800

    Add lr.

--- configs/pascal_voc/faster_rcnn_r50_fpn_1x_cell_voc.py
+++ configs/pascal_voc/faster_rcnn_r50_fpn_1x_cell_voc.py
@@ -4,7 +4,7 @@ _base_ = [
 ]
 model = dict(roi_head=dict(bbox_head=dict(num_classes=33)))
 # optimizer
-optimizer = dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 # actual epoch = 3 * 3 = 9
