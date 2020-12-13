b519dc0d586183b912bfeff49f5e9c4ec8d4f93d
liyinhao <liyinhao0413@qq.com>
2020-12-13 14:09:59 +0800

    Add chromosome_class config, able to train class.

--- configs/_base_/datasets/chromosome_class.py
+++ configs/_base_/datasets/chromosome_class.py
@@ -1,9 +1,9 @@
 # dataset settings
 dataset_type = 'VOCDataset'
-data_root = 'data/karyotype/chromosome_count/'
+data_root = 'data/karyotype/chromosome_class/'
 img_norm_cfg = dict(
     mean=[224.35568854, 224.35568854, 224.35568854], std=[27.83740616, 27.83740616, 27.83740616], to_rgb=True)
-classes = ['chromosome']
+classes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y']
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
